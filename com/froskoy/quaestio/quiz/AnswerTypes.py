7f69a5f8acad3c5a926cc50e16611c8982c25a5e
Matthew Ireland <matthew@mti.me.uk>
2014-08-19 15:18:25 +0100

    Improved error handling and also mapping for true/false questions/answers.

--- com/froskoy/quaestio/quiz/AnswerTypes.py
+++ com/froskoy/quaestio/quiz/AnswerTypes.py
@@ -6,11 +6,16 @@ Created on 18 Aug 2014
 Equivalent of QuestionTypes.py, for answers.
 '''
 
+import TrueFalseQuestionAnswer as TFQA
 import MultipleChoiceQuestionAnswer as MCQA
 import ShortAnswerQuestionAnswer as SAQA
+from com.froskoy.quaestio.jsonSerializing.WrongAnswerTypeError import WrongAnswerTypeError
 
 def map(answerType):
+    if (answerType == TFQA.TrueFalseQuestionAnswer):
+        return 'truefalsequestionanswer'
     if (answerType == MCQA.MultipleChoiceQuestionAnswer):
         return 'multiplechoicequestionanswer'
     if (answerType == SAQA.ShortAnswerQuestionAnswer):
         return 'shortanswerquestionanswer'
+    raise WrongAnswerTypeError(answerType+" is not a supported answer type.")
--- com/froskoy/quaestio/quiz/QuestionTypes.py
+++ com/froskoy/quaestio/quiz/QuestionTypes.py
@@ -4,11 +4,16 @@ Created on 17 Aug 2014
 @author: matthew
 '''
 
+import TrueFalseQuestion as TFQ
 import MultipleChoiceQuestion as MCQ
 import ShortAnswerQuestion as SAQ
+from com.froskoy.quaestio.jsonSerializing.WrongQuestionTypeError import WrongQuestionTypeError
 
 def map(questionType):
+    if (questionType == TFQ.TrueFalseQuestion):
+        return 'truefalse'
     if (questionType == MCQ.MultipleChoiceQuestion):
         return 'multiplechoice'
     if (questionType == SAQ.ShortAnswerQuestion):
         return 'shortanswer'
+    raise WrongQuestionTypeError(questionType+" is not a supported question type.")

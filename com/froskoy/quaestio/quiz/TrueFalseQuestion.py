2cafbba9ae51e8a481210386288a36e16e57e8df
Matthew Ireland <matthew@mti.me.uk>
2014-08-19 15:18:57 +0100

    Improved sanity of internal representation for true/false questions.

--- com/froskoy/quaestio/quiz/TrueFalseQuestion.py
+++ com/froskoy/quaestio/quiz/TrueFalseQuestion.py
@@ -7,30 +7,56 @@ Issues: answer ordering (why not just have answers input as a list and add a shu
 '''
 
 from Question import Question
+import QuestionTypes
 from TrueFalseQuestionAnswer import TrueFalseQuestionAnswer
 from com.froskoy.quaestio.jsonSerializing.WrongQuestionTypeError import WrongQuestionTypeError
+from com.froskoy.quaestio.jsonSerializing.JsonError import JsonError
 
 class TrueFalseQuestion(Question):
     '''
     classdocs
     '''
 
-    def __init__(self, questionText="", correctAnswer=None, wrongAnswer=None, name=None, jsonDict=None):
+    def __init__(self, questionText="", isTrue=0, correctFeedback=None, incorrectFeedback=None, name=None, jsonDict=None):
         '''
         Constructor
         '''
         if (jsonDict is None):
             super(TrueFalseQuestion,self).__init__(questionText=questionText,name=name)
-            self.__answers = [correctAnswer, wrongAnswer]
+            self.__isTrue=isTrue
+            self.__correctAnswerFeedback = correctFeedback
+            self.__wrongAnswerFeedback   = incorrectFeedback
+            
+            # self.__answers is *ONLY* used in converting to/from a json dict
+            if (isTrue):
+                correctAnswer   = TrueFalseQuestionAnswer(true=1, correct=1, feedback=correctFeedback,   penalty=0, hidden=0)
+                incorrectAnswer = TrueFalseQuestionAnswer(true=0, correct=0, feedback=incorrectFeedback, penalty=0, hidden=0)
+            else:
+                correctAnswer   = TrueFalseQuestionAnswer(true=0, correct=1, feedback=correctFeedback,   penalty=0, hidden=0)
+                incorrectAnswer = TrueFalseQuestionAnswer(true=1, correct=0, feedback=incorrectFeedback, penalty=0, hidden=0)
+            self.__answers = [correctAnswer, incorrectAnswer]
+
         else:
-            if (jsonDict['type'] != "truefalse"):
+            if (jsonDict['type'] != QuestionTypes.map(self.__class__)):
                 raise WrongQuestionTypeError("Attempting to construct truefalse question from dictionary whose type element is not set to truefalse.")
             qName = jsonDict['name'] if (jsonDict['name'] != "") else None
             super(TrueFalseQuestion,self).__init__(questionText=jsonDict['questiontext'],name=qName)
             
+            # self.__answers is *ONLY* used in converting to/from a json dict
             self.__answers = []
             for answer in jsonDict['answers']:
-                self.__answers.append(TrueFalseQuestionAnswer(jsonDict=answer))
+                answer = TrueFalseQuestionAnswer(jsonDict=answer)
+                if (answer.isCorrect()):
+                    self.__correctAnswerFeedback = answer.getFeedback()
+                    if (answer.isTrue()):
+                        self.__isTrue=1
+                    else:
+                        self.__isTrue=0
+                elif (answer.isWrong()):
+                    self.__incorrectAnswerFeedback = answer.getFeedback()
+                else:
+                    raise JsonError("answer is neither correct nor incorrect... something's gone wrong")
+                self.__answers.append(answer)
             assert(len(self.__answers) == 2)
         
     def toJsonDict(self):
@@ -41,5 +67,5 @@ class TrueFalseQuestion(Question):
                 'answers'      : map(lambda x : x.toJsonDict(), self.__answers)
                }
     
-    def markAnswer(self):
+    def markAnswer(self, answer):
         raise NotImplementedError() 
--- com/froskoy/quaestio/quiz/TrueFalseQuestionAnswer.py
+++ com/froskoy/quaestio/quiz/TrueFalseQuestionAnswer.py
@@ -5,6 +5,7 @@ Created on 17 Aug 2014
 '''
 
 from Answer import Answer
+import AnswerTypes
 from com.froskoy.quaestio.jsonSerializing.WrongAnswerTypeError import WrongAnswerTypeError
 
 class TrueFalseQuestionAnswer(Answer):
@@ -13,17 +14,23 @@ class TrueFalseQuestionAnswer(Answer):
     '''
 
 
-    def __init__(self, text="", correct=0, feedback=None, penalty=0, hidden=0, jsonDict=None):
+    def __init__(self, true=0, correct=0, feedback=None, penalty=0, hidden=0, jsonDict=None):
         '''
         Constructor
         '''
         if (jsonDict is None):
-            if (not isinstance(text, str)):
-                raise ValueError("Question text must be of type string")
+            if (not isinstance(true, int)):
+                raise ValueError("'true' must be of type int (non-zero values indicate a true answer)")
             if (not isinstance(correct, int)):
-                raise ValueError("Correct must be of type int (non-zero values indicate a correct answer)")
+                raise ValueError("'correct' must be of type int (non-zero values indicate a correct answer)")
         
-            self.__text = text
+            self.__true = true
+            if (true == 0):
+                self.__text = "false"
+            else:
+                self.__text = "true"
+                self.__true = self.__true / self.__true
+
             if (correct == 0):
                 self.__correct = correct
             else:
@@ -33,13 +40,20 @@ class TrueFalseQuestionAnswer(Answer):
             self.__penalty  = penalty
             self.__hidden   = hidden
         else:
-            if (jsonDict['type'] != "truefalsequestionanswer"):
+            if (jsonDict['type'] != AnswerTypes.map(self.__class__)):
                 raise WrongAnswerTypeError("Attempting to construct truefalse answer from dictionary whose type element is not set to truefalsequestionanswer.")
             self.__text     = jsonDict['text']
             self.__correct  = int(jsonDict['correct'])
             self.__feedback = jsonDict['feedback'] if (jsonDict['feedback'] != "") else None
             self.__penalty  = jsonDict['penalty']
             self.__hidden   = jsonDict['hidden']
+            
+            if   (self.__text == "true"):
+                self.__true=1
+            elif (self.__text == "false"):
+                self.__true=0
+            else:
+                raise WrongAnswerTypeError("true/false question answer text must be equal to 'true' or 'false'")
         
     def toJsonDict(self):
         feedback = self.__feedback if (self.__feedback is not None) else ""
@@ -49,3 +63,21 @@ class TrueFalseQuestionAnswer(Answer):
                 'feedback' : feedback,
                 'penalty'  : self.__penalty,
                 'hidden'   : self.__hidden}
+        
+    def getFeedback(self):
+        return self.__feedback
+        
+    def isTrue(self):
+        return self.__true
+    
+    def isFalse(self):
+        return (not self.__true)
+    
+    def isCorrect(self):
+        return self.__correct
+    
+    def isIncorrect(self):
+        return (not self.__correct)
+    
+    def isWrong(self):
+        return (self.isIncorrect())
